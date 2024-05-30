b51b9e621cad0db74a5c94196a2f0ac0a135457e
zoanna <zoanna5442@gmail.com>
2024-05-30 09:34:34 +0900

    Changes

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+
+    /**
+    * @author 조아라
+     * @deprecated 사용하지 않는 기능
+     * @apiNote  무엇을 하는 기능입니다
+    */
+    public static void main(String[] args){
+
+        int a = 10;
+        System.out.println(a);
+
+    }
+}
