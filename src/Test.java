d8eb121e53d313668974ac733385cadb376c827d
zoanna <zoanna5442@gmail.com>
2024-06-03 22:24:51 +0900

    Learning the Java 5/31

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,99 @@
 public class Test {
 
-    /**
-    * @author 조아라
-     * @deprecated 사용하지 않는 기능
-     * @apiNote  무엇을 하는 기능입니다
+  /**
+   * @author 조아라
+   * @deprecated 사용하지 않는 기능
+   * @apiNote 무엇을 하는 기능입니다
+   */
+  public static void main(String[] args)  {
+    /*int a = 10;
+    int b = 50;
+    int c = 40;
+    int result = a + b + c;
+    System.out.println(result);*/
+
+    // 정수형 자료형
+    /*int a = 1;
+    byte b = 2;
+    long c = 3;
+    short d = 4;
+
+    // 실수형 자료형
+    float e = 0.1f; // float은 f를 붙힌다.
+    double f = 0.2;
+    double g = 0.2d; // d가 없어도 상관 없다*/
+
+    // 문자 자료형
+    /*char a = 'a';
+
+    String str = "hell";*/
+
+    /*int a = 2147483648;
+    long b = 9223372036854775808;
+    short c = -40000;
+    byte = -200;*/
+
+    // 원시타입은 스택 메모리에 저장되어 접근이 빠르다.
+    /* int a = 1;
+    int v = a;
+    System.out.println(a);
+    System.out.println(v);*/
+
+    /*char c1 = 'A';
+    char c2 = 65;
+    char c3 = '\u0041';
+
+    char c4 = '가';
+    char c5 = 44032;
+    char c6 = '\uac00';
+
+    int uniCode = c1;
+
+    System.out.println(c1);
+    System.out.println(c2);
+    System.out.println(c3);
+    System.out.println(c4);
+    System.out.println(c5);
+    System.out.println(c6);
+    System.out.println(uniCode);*/
+
+    /*short sVal = 10;
+    short result = (short) (sVal + 10);
+    System.out.println(result);*/
+
+    /*float myFloat = 9.75f;
+      float anotherFloat = myFloat * 2;
+      System.out.println("original myFloat : " + myFloat);
+      System.out.println("original anotherFloat : " + anotherFloat);
     */
-    public static void main(String[] args){
 
-        int a = 10;
-        System.out.println(a);
+    /*boolean isTrue = true;
+    if(isTrue) {
+      System.out.println("이건 트루입니다");
+    } else {
+      System.out.println("이건 펄스입니다");
+    }*/
 
+    /*test();
+    public static void test(){
+      System.out.println("hello world");
     }
+  */
+
+/*  @Override
+  public void test() {
+
+  }*/
+
+  /*int[] arr = {1,2,3,4,5};
+  int[] arr2 = new int[5];
+    System.out.println(arr);
+*/
+
+ /* String name = "ara";
+  String name2 = new String();*/
+
+  double[] num = new double[100];
+    System.out.println(num[0]);
+  }
 }
