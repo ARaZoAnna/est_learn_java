b849cb65ff7324682108c89f097841d9d3b15cca
zoanna <zoanna5442@gmail.com>
2024-06-20 19:38:50 +0900

    Ormi 5기 6/20 수업 중 코드

--- java0620/src/Main.java
+++ java0620/src/Main.java
@@ -8,13 +8,37 @@ import java.util.stream.Stream;
 
 public class Main {
   public static void main(String[] args) throws InterruptedException {
+
+
+    //예제3
+    /*String sentence = "Java Stream API Example";
+
+    long wordCount = Arrays.stream(sentence.split(" "))
+            .count();
+    System.out.println("단어 수 : " + wordCount);*/
+
+    //예제2
+    /*List<String> words = Arrays.asList("Java", "Stream", "API", "Example");
+
+    List<Integer> wordLengths = words.stream()
+            .map(String::length)
+            .collect(Collectors.toList());
+    System.out.println("문자열 길이: " + wordLengths);*/
+
+    //예제1
+    /*List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+    List<Integer> odd = numbers.stream().filter(integer -> integer % 2 == 1).toList();
+
+    System.out.println(odd);*/
+
     /*ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(3,1,10,9,5,20));*/
 
-    /*numbers.stream().filter(integer -> integer % 2 == 0);
+    /* numbers.stream().filter(integer -> integer % 2 == 0);
     numbers.stream().map(integer -> integer*2);
-    numbers.stream().sorted();*/
+    numbers.stream().sorted();
 
-    /*numbers.stream()
+    numbers.stream()
     .filter(integer -> integer % 2 == 0)
     .forEach(integer -> System.out.println(integer));*/
 
@@ -28,12 +52,20 @@ public class Main {
     /*numbers.stream().sorted().forEach(integer -> System.out.println(integer));*/
 
     List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-
-    List<Integer> num =
+    // 문제 1
+    /*List<Integer> num =
         numbers.stream()
             .filter(integer -> integer % 2 == 1)
             .collect(Collectors.toList());
-    System.out.println(num);
+    System.out.println(num);*/
+
+    // 문제2
+    numbers.stream().filter(integer -> integer <= 5).forEach(integer -> System.out.println(integer));
+
+    /*List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+    int sum = numbers.stream()
+            .reduce(0, (a,b) -> a+b);
+    System.out.println(sum);*/
   }
 }
 
