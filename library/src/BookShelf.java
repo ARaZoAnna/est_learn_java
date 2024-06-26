ca80a0b52331aeb8c37e80c2a5e56ab2f09f37ae
zoanna <zoanna5442@gmail.com>
2024-06-26 22:22:28 +0900

    Ormi 5기 도서관 과제

---
+++ library/src/BookShelf.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookShelf<T> implements BookManager<T>{
+    private List<Book<T>> books;
+
+    public BookShelf() {
+        this.books = new ArrayList<>();
+    }
+
+    @Override
+    public void addBook(Book<T> book) {
+        this.books.add(book);
+    }
+
+    @Override
+    public void removeBook(Book<T> book) {
+        this.books.remove(book);
+    }
+
+    @Override
+    public List<Book<T>> searchByTitle(String title) {
+        List<Book<T>> searchingTitle = new ArrayList<>();
+        //리스트가 비어있다면
+        if (books.isEmpty()){
+            System.out.println("");
+        }else{
+            //대문자로 변환하여 비교한다
+            String upperTitle = title.toUpperCase();
+            for(int i = 0; i < books.size(); i++){
+                //대문자로 변환하여 비교한다
+                String upperBooksTitle = books.get(i).getTitle().toUpperCase();
+                //부분 일치하는 제목도 검색 결과에 포함하기 위해 contains함수 사용
+                if(upperBooksTitle.contains(upperTitle)){
+                    searchingTitle.add(books.get(i));
+                }
+            }
+        }
+        return searchingTitle;
+    }
+
+    @Override
+    public List<Book<T>> searchByAuthor(String author) {
+        List<Book<T>> searchingAuthor = new ArrayList<>();
+        //리스트가 비어있다면
+        if (books.isEmpty()){
+            System.out.println("");
+        }else {
+            //대문자로 변환하여 비교한다.
+            String upperAuthor = author.toUpperCase();
+            for(int i = 0;i<books.size(); i++){
+                //대문자로 변환하여 비교한다
+                String upperBooksTitle = books.get(i).getTitle().toUpperCase();
+                //부분 일치하는 저자도 검색 결과에 포함하기 위해 contains함수 사용
+                if(upperBooksTitle.contains(upperAuthor)){
+                    searchingAuthor.add(books.get(i));
+                }
+            }
+        }
+        return searchingAuthor;
+    }
+}
